efdc517a53bbb277fe642a6dfb1cf422239f3d32
lqp861027 <104400830+lqp861027@users.noreply.github.com>
2024-04-22 13:21:28 +0800

    Update itv_all.py

--- itv_all.py
+++ itv_all.py
@@ -284,7 +284,7 @@ results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
 results.sort(key=lambda x: channel_key(x[0]))
 
 
-result_counter = 8  # 每个频道需要的个数
+result_counter = 5  # 每个频道需要的个数
 
 with open("itvlist.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
